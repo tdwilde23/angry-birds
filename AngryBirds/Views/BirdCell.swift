a242cd69e5a56f12afa2a042b78bb7fec7e5693b
Taylor Wilde <taylorwilde@Taylors-MacBook-Pro.local>
2022-04-25 11:59:23 -0500

    fixed BirdCell to handle bad image urls

--- AngryBirds/Views/BirdCell.swift
+++ AngryBirds/Views/BirdCell.swift
@@ -23,7 +23,11 @@ class BirdCell: UITableViewCell {
             DispatchQueue.global(qos: .userInitiated).async {
                 let birdImageData = NSData(contentsOf: URL(string: self.bird!.imageUrl)!)
                 DispatchQueue.main.async {
-                    self.birdImageView.image = UIImage(data: birdImageData as! Data)
+                    if birdImageData == nil {
+                        self.birdImageView.image = UIImage(systemName: "face.smiling")
+                    } else {
+                        self.birdImageView.image = UIImage(data: birdImageData as! Data)
+                    }
                     self.birdImageView.layer.cornerRadius = self.birdImageView.frame.width / 2
                 }
             }
